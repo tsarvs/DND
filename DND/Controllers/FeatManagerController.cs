7989f7c3d94fbf8ada842cd456ef61ce0201e4b8
Thomas F Sarver <tfsarv01@louisville.edu>
2019-05-21 12:23:57 -0500

    getting feat manager to query db less

--- DND/Controllers/FeatManagerController.cs
+++ DND/Controllers/FeatManagerController.cs
@@ -22,6 +22,10 @@ namespace DND.Controllers
 
         private int _characterId;
 
+        private List<FEATS> _loadedDbFeats;
+
+        private List<FEATS> _loadedCharacterFeats;
+
         #endregion
 
         #region Constructor
@@ -31,6 +35,16 @@ namespace DND.Controllers
             _view = view;
             _madeChanges = false;
             _characterId = 0;
+
+            using (var db = new DragonDBModel())
+            {
+                _loadedDbFeats = db.FEATS.ToList();
+
+                _loadedCharacterFeats =
+                    (from c in db.CHARACTER.ToList()
+                     where (c.c_id == _characterId)
+                     select c.FEATS.FirstOrDefault()).ToList();
+            }
         }
 
         #endregion
@@ -40,27 +54,17 @@ namespace DND.Controllers
         public void PopulateControls(int characterId)
         {
             _characterId = characterId;
+            
+            _view.FeatsListBox.DisplayMember = "f_name";
+            _view.FeatsListBox.ValueMember = "f_id";
+            _view.FeatsListBox.SelectedItem = null;
 
-            using (var db = new DragonDBModel())
-            {
-                //lbCharacterFeats
-                var characterFeatsQuery =
-                    (from f in db.FEATS.ToList()
-                        where (f.CHARACTER.Any(x => x.c_id == _characterId))
-                        select f).ToList();
-                
-                _view.FeatsListBox.DisplayMember = "f_name";
-                _view.FeatsListBox.ValueMember = "f_id";
-                _view.FeatsListBox.SelectedItem = null;
-                
-                _view.CharacterFeatsListBox.DisplayMember = "f_name";
-                _view.CharacterFeatsListBox.ValueMember = "f_id";
-                _view.CharacterFeatsListBox.SelectedItem = null;
+            _view.CharacterFeatsListBox.DisplayMember = "f_name";
+            _view.CharacterFeatsListBox.ValueMember = "f_id";
+            _view.CharacterFeatsListBox.SelectedItem = null;
 
-                //lbFeats.DataSource is set/updated thru here
-                UpdateListsContents(characterFeatsQuery);
-
-            }
+            //lbFeats.DataSource is set/updated thru here
+            UpdateListsContents(_loadedCharacterFeats);
         }
 
         public void ReloadFeatList()
@@ -75,45 +79,18 @@ namespace DND.Controllers
 
         public void UpdateFeatDisplay(bool selectedFeat)
         {
-            using (var db = new DragonDBModel())
-            {
-                int featId;
-                FEATS feat;
-                
-                if (selectedFeat)
-                {
-                    feat = (FEATS)_view.FeatsListBox.SelectedItem;
-                }
-                else
-                {
-                    feat = (FEATS)_view.CharacterFeatsListBox.SelectedItem;
-                }
+            FEATS feat;
 
-                if (feat == null)
-                    return;
+            if (selectedFeat)
+                feat = (FEATS) _view.FeatsListBox.SelectedItem;
+            else
+                feat = (FEATS) _view.CharacterFeatsListBox.SelectedItem;
 
+            if (feat == null)
+                return;
 
-                featId = feat.f_id;
-
-                var featQuery =
-                    (from f in db.FEATS.ToList()
-                        where (f.f_id == featId)
-                        select new
-                        {
-                            Source = f.f_source,
-                            Description = f.f_description
-                        }).FirstOrDefault();
-
-                featQuery = featQuery ?? new
-                {
-                    Source = "",
-                    Description = ""
-                };
-
-                _view.FeatSource.Text = featQuery.Source;
-                _view.FeatDescription.Text = featQuery.Description;
-
-            }
+            _view.FeatSource.Text = feat.f_source;
+            _view.FeatDescription.Text = feat.f_description;
         }
         
         public void AddFeatToCharacter()
@@ -144,26 +121,7 @@ namespace DND.Controllers
             if (!_madeChanges)
                 return;
             
-            using (var db = new DragonDBModel())
-            {
-                var inDbCharacter = db.CHARACTER.Single(x => x.c_id == _characterId);
-                          
-                var inFormCharacterFeats = _view.CharacterFeatsListBox.Items.Cast<FEATS>().ToList();
-                
-                inDbCharacter.FEATS.Clear();
-
-                foreach (var feat in inFormCharacterFeats)
-                {
-                    var inDbFeat =
-                        (from f in db.FEATS.ToList()
-                        where (f.f_id == feat.f_id)
-                        select f).SingleOrDefault();
-
-                    inDbCharacter.FEATS.Add(inDbFeat);
-                }
-
-                db.SaveChanges();
-            }
+            
         }
 
         public void NewFeat()
@@ -191,21 +149,11 @@ namespace DND.Controllers
 
         private void UpdateListsContents(List<FEATS> characterFeats)
         {
-            List<FEATS> featsQuery;
-
             //refresh feats list
-            using (var db = new DragonDBModel())
-            {
-                featsQuery =
-                    (from f in db.FEATS.ToList()
-                     select f).ToList();
-            } 
+            var featsQuery = _loadedDbFeats;
 
             //update feats list by removing the feats the character already has
-            foreach (var feat in characterFeats)
-            {
-                featsQuery.Remove(featsQuery.Find(x => x.f_id == feat.f_id));
-            }
+            foreach (var feat in characterFeats) featsQuery.Remove(featsQuery.Find(x => x.f_id == feat.f_id));
 
             _view.CharacterFeatsListBox.DataSource = characterFeats;
             _view.FeatsListBox.DataSource = featsQuery;
