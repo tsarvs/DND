37098bb0e9176edf7eacb71849db1d098a44a719
tylerdrobnik <tylerdrobnik@gmail.com>
2019-05-22 13:17:06 -0500

    added Journal form and interface

---
+++ DND/Views/Interfaces/ICharacterJournalForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DND.Views.Interfaces
+{
+    interface ICharacterJournalForm
+    {
+        String JournalBody { get; set; }
+        String PageNumber { get; set; }
+        Button PrevPage { get; set; }
+        Button NextPage { get; set; }
+        Button AddEntry { get; set; }
+        Button DeleteEntry { get; set; }
+
+        //void SetController(CharacterJournalController controller);
+
+    }
+}
