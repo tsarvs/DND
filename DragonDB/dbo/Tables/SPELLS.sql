3ab8e959d889e2972b0fdb5c8a52f93163a17980
tsarvs <tfsarv01@louisville.edu>
2020-03-05 23:53:07 -0600

    made changes to SPELLS table structure to better reflect the JSON it gathered information from

--- DragonDB/dbo/Tables/SPELLS.sql
+++ DragonDB/dbo/Tables/SPELLS.sql
@@ -3,14 +3,13 @@
     [s_name]               VARCHAR (250) NULL,
     [s_school]             VARCHAR (100) NULL,
 	[s_level]			   INT			 NULL,
-    [s_castingtime] VARCHAR(100)           NULL,
-    [s_range]              VARCHAR (10)  NULL,
-    [s_target]             VARCHAR (100) NULL,
+    [s_castingtime] VARCHAR(250)           NULL,
+    [s_range]              VARCHAR (100)  NULL,
     [s_component_v]        BIT           NULL,
     [s_component_s]        BIT           NULL,
-    [s_component_m]        VARCHAR (250) NULL,
+    [s_component_m]        VARCHAR (500) NULL,
     [s_isconcentration]    BIT           NULL,
-    [s_durationminutes]    VARCHAR(100)           NULL,
+    [s_duration]    VARCHAR(100)           NULL,
     [s_description] VARCHAR(MAX) NULL, 
     PRIMARY KEY CLUSTERED ([s_id] ASC)
 );
---
+++ DragonDB_JSON_Parser_Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonDB_JSON_Parser_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
